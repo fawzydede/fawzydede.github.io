b46be37c2496a61619d4974b5d98b8bb2d7b1cb5
fawzydede <dede.fawzy@gmail.com>
2024-07-25 15:22:04 +0700

    tries to make money

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ['./index.html',
+            './property/index.html'],
   theme: {
     extend: {},
   },
